a4928c5110cc6da5ed8e452020dc13d7d14e1966
seo-faper <dswhdcks@gmail.com>
2023-04-30 08:38:49 +0900

    init plugins

---
+++ src/kr/ac/cju/cifrar/DeathPosition.java
@@ -0,0 +1,16 @@
+package kr.ac.cju.cifrar;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DeathPosition extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("plugin Enable ");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("plugin Disable");
+    }
+}
